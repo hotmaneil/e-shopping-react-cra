44e2748834fc6aef67e3883d2c6339648739ba90
Neil.Tsai <Neil.Tsai@sunstige.com>
2025-03-20 09:59:02 +0800

    目前進行到；路由保護

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/admin/Dashboard'
+import AdminProducts from './pages/admin/AdminProducts'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="products" element={<AdminProducts />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
